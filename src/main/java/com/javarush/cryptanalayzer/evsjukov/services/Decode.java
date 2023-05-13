389463bc97a0bdd12db15af79db7a62611a4d009
Sergey Evsjukov <s.evsjukov@gmail.com>
2023-05-13 22:20:20 +0400

    Files commit

--- src/main/java/com/javarush/cryptanalayzer/evsjukov/services/Decode.java
+++ src/main/java/com/javarush/cryptanalayzer/evsjukov/services/Decode.java
@@ -1,4 +1,7 @@
 package com.javarush.cryptanalayzer.evsjukov.services;
-
 public class Decode {
+    public static void main(String[] args) {
+        String text = "input.txt";
+        System.out.println(text);
+    }
 }
