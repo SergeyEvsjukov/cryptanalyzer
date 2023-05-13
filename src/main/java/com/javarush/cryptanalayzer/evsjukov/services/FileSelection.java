db3a18e00dcfed7e20fb89749f3280c50f9b06b7
Sergey Evsjukov <s.evsjukov@gmail.com>
2023-05-13 22:21:00 +0400

    Files commit

---
+++ src/main/java/com/javarush/cryptanalayzer/evsjukov/services/FileSelection.java
@@ -0,0 +1,15 @@
+package com.javarush.cryptanalayzer.evsjukov.services;
+
+import java.util.Scanner;
+
+public class FileSelection {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String FileName;
+
+        public static class FileName {
+            String fileName;
+        }
+    }
+}
