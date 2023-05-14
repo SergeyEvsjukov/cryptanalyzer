873b267946c459bb36f5cb26fc011a25af99f197
Sergey Evsjukov <s.evsjukov@gmail.com>
2023-05-14 23:44:44 +0400

    Files commit

--- src/main/java/com/javarush/cryptanalayzer/evsjukov/services/Encode.java
+++ src/main/java/com/javarush/cryptanalayzer/evsjukov/services/Encode.java
@@ -7,14 +7,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Scanner;
-
 public class Encode {
     public static void main(String[] args) {
-        System.out.println("Введите ключ для шифрования от 1 до " + (CryptoAlphabet.keyLimits+1));
-        System.out.println("Или нажмите \"Пробел\" для получения случайного ключа для ширования");
+        System.out.println("Введите ключ для шифрования от 1 до " + (CryptoAlphabet.keyLimits + 1));
+        System.out.println("Или нажмите \"0\" для получения случайного ключа для шифрования");
         Scanner scanner = new Scanner(System.in);
-        ChoiseKey key = new ChoiseKey();
+        ChoiseKey key;
+        int inputKey = scanner.nextInt();
+        if (inputKey > 0 && inputKey < CryptoAlphabet.keyLimits) {
+           key = new ChoiseKey();
+           key.setKey(inputKey);
+        } else if (inputKey==0) {
+            key = new ChoiseKey();
+        } else {
+            System.out.println("Введен ключ вне указанного диапазона");
+        }
         System.out.println(key.getKey());
+
 
 //        ArrayList<Character> input = new ArrayList<Character>();
 //        try(FileReader reader = new FileReader("input.txt"))
@@ -32,5 +41,6 @@ public class Encode {
 //            System.out.println(ex.getMessage());
 //        }
 
-    }
+        }
 }
+
